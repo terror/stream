03e39214b8af85d7d2765160d2ce9f5d83b6bd00
Liam <liam@scalzulli.com>
2023-03-30 18:41:44 -0400

    Change vite proxy route

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,9 @@ export default defineConfig({
   build: { assetsDir: '.' },
   server: {
     proxy: {
-      '/api': {
+      '/': {
         target: 'http://127.0.0.1:8000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
