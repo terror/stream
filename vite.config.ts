6d6dd95f362b4560eb1f6ff048a37c5e97362788
Liam <liam@scalzulli.com>
2023-03-30 18:34:07 -0400

    Move tsconfig.json files into root

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: './client',
   plugins: [react()],
+  build: { assetsDir: '.' },
   server: {
     proxy: {
       '/api': {
