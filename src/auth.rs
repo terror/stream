bdd2471e46f2a480ceb20ffc64b267346f0f52e5
Liam <liam@scalzulli.com>
2023-03-31 11:51:16 -0400

    Try making a synchronous request for a token

--- src/auth.rs
+++ src/auth.rs
@@ -39,8 +39,7 @@ pub(crate) async fn authorized(
   let token = state
     .oauth_client
     .exchange_code(AuthorizationCode::new(query.code))
-    .request_async(async_http_client)
-    .await?;
+    .request(http_client)?;
 
   let mut session = Session::new();
 
